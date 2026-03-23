eb52553792be356d8e88549be0c453b8b28b05d4
Lauren Frisbie <lauren.frisbie@doh.wa.gov>
2026-03-23 13:27:58 -0700

    Fixing error where ncbi datasets downloading points to

--- ncov_wa/scripts/pull_ncbi_wa_data.sh
+++ ncov_wa/scripts/pull_ncbi_wa_data.sh
@@ -7,13 +7,13 @@ chmod +x datasets dataformat
 
 
 echo "Downloading WA SARS-CoV-2 sequences from NCBI..."
-datasets download virus genome taxon sars-cov-2 --usa-state WA --filename ./data/wa_sars2.zip
+./datasets download virus genome taxon sars-cov-2 --usa-state WA --filename ./data/wa_sars2.zip
 
 unzip ./data/wa_sars2.zip -d ./data/wa_sars2
 
 cp ./data/wa_sars2/ncbi_dataset/data/genomic.fna ./data/wa_sars2/ncbi_dataset/data/wa_sequences.fasta
 
 #pull all fields in ncbi dataset
-dataformat tsv virus-genome --inputfile ./data/wa_sars2/ncbi_dataset/data/data_report.jsonl > ./data/wa_sars2/ncbi_dataset/data/wa_metadata_raw.tsv
+./dataformat tsv virus-genome --inputfile ./data/wa_sars2/ncbi_dataset/data/data_report.jsonl > ./data/wa_sars2/ncbi_dataset/data/wa_metadata_raw.tsv
 
 echo "NCBI WA metadata and sequences downloaded"
